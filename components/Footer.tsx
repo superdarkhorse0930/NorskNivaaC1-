ba52c12638b9d0ada4b67bb639465935cb6d5fb1
Diabolomika <diabolomika@gmail.com>
2024-07-03 11:19:28 +0200

    Create Footer.tsx
    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,47 @@
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-3 space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+          <SocialIcon kind="x" href={siteMetadata.x} size={6} />
+          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
+          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
+        </div>
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/diabolomika/..tailwind-nextjs-blog">
+            Norskprøve C1
+          </Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
+###### CONTACT
+
+*   Smedsbuktveien 20, Kragerø - 3370, Telemark,Norge
+    
+*   - - -
+    
+    [diabolomika@gmail.com](mailto:diabolomika@gmail.com)  
+    [+47 40088896](tel:+47 400 88896)  
+    
+    [](#)[](#)[](#)[](#)
+    
+    [Privacy](#) | [Terms](#) | [Site Map](#)
